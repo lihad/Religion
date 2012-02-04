9f4ad2413a71afc0deec41aad7eb2c4c7bcdb708
Joren <GitHub.Joren@Joren.me>
2012-02-05 07:46:42 +0900

    isSameBlock comparison function, make the Util class use static methods

--- src/Lihad/Religion/Util/Util.java
+++ src/Lihad/Religion/Util/Util.java
@@ -2,12 +2,27 @@ package Lihad.Religion.Util;
 
 import java.util.List;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import Lihad.Religion.Information.BeyondInfo;
 
+/**
+ * 
+ * Utility class, holds static convenience methods
+ * 
+ * @author Lihad
+ * @author Joren
+ *
+ */
 public class Util {
-	public void towerBroadcast(String towerName, String message)
+
+	/**
+	 * Broadcasts a message to all players who are part of specified Tower
+	 * @param towerName - name of the target Tower
+	 * @param message - the message to be broadcast
+	 */
+	public static void towerBroadcast(String towerName, String message)
 	{
 		List<Player> players = BeyondInfo.getTowerPlayers(towerName);
 		for (Player player: players)
@@ -16,12 +31,30 @@ public class Util {
 		}
 	}
 	
-	public void religionBroadcast(String religionName, String message)
+	/**
+	 * Broadcasts a message to all players who are part of specified Religion
+	 * @param religionName - name of the target religion
+	 * @param message - the message to be broadcast
+	 */
+	public static void religionBroadcast(String religionName, String message)
 	{
 		List<Player> players = BeyondInfo.getReligionPlayers(religionName);
 		for (Player player: players)
 		{
 			player.sendMessage(message);
 		}
+	}
+	
+	/**
+	 * Compares two locations to see if they share the same block.
+	 * 
+	 * @param a - Location to be compared with b
+	 * @param b - Location to be compared with a
+	 * @return true if they share the same block, false if not, false if either Location is null
+	 */
+	
+	public static boolean isSameBlock(Location a, Location b)
+	{
+		return (a!=null && b!=null && a.getBlock().equals(b.getBlock()));
 	}
 }
