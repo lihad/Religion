964c36a35ca56080da9981f3b578f617c1e74378
Kyle <kylecubed@comcast.net>
2012-02-04 13:38:04 -0800

    Changed CommandRunner to Static BeyondInfo

--- src/Lihad/Religion/Command/CommandRunner.java
+++ src/Lihad/Religion/Command/CommandRunner.java
@@ -1,6 +1,5 @@
 package Lihad.Religion.Command;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,10 +7,10 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
 import Lihad.Religion.Religion;
+import Lihad.Religion.Information.BeyondInfo;
 
 
 
@@ -46,8 +45,8 @@ public class CommandRunner implements CommandExecutor {
 		 * Base Command, drives /rr.  Tells the player what religion and tower they are member of
 		 */
 		if(cmd.getName().equalsIgnoreCase("rr") && arg.length == 0){
-			if(Religion.info.getTowerName((Player)sender) == null) sender.sendMessage("You are not a member of any religion");
-			else sender.sendMessage("You are a member of "+Religion.info.getTowerName((Player)sender)+", a tower of "+Religion.info.getReligion((Player)sender));
+			if(BeyondInfo.getTowerName((Player)sender) == null) sender.sendMessage("You are not a member of any religion");
+			else sender.sendMessage("You are a member of "+BeyondInfo.getTowerName((Player)sender)+", a tower of "+BeyondInfo.getReligion((Player)sender));
 			return true;
 		}
 		
@@ -56,16 +55,16 @@ public class CommandRunner implements CommandExecutor {
 		 * Drives '/rr join <param>' command.  This allows a player to join the chosen tower. 
 		 */
 		else if(cmd.getName().equalsIgnoreCase("rr") && arg[0].equals("join") && arg.length == 2){
-			if(!Religion.info.hasTower(arg[1])){
+			if(!BeyondInfo.hasTower(arg[1])){
 				sender.sendMessage("Invalid tower you fuck head");
 				return true;
 			}
-			if(Religion.info.hasPlayer((Player)sender)){
+			if(BeyondInfo.hasPlayer((Player)sender)){
 				sender.sendMessage("You are already a member of a Tower. Please leave your current one to join this one.");
 				return true;
 			}
-			Religion.info.addPlayer((Player)sender, Religion.info.getReligion(arg[1]), arg[1]);
-			sender.sendMessage("You have joined the Tower of "+arg[1]+" and the Religion of "+Religion.info.getReligion(arg[1]));
+			BeyondInfo.addPlayer((Player)sender, BeyondInfo.getReligion(arg[1]), arg[1]);
+			sender.sendMessage("You have joined the Tower of "+arg[1]+" and the Religion of "+BeyondInfo.getReligion(arg[1]));
 			return true;
 		}
 		
@@ -73,11 +72,11 @@ public class CommandRunner implements CommandExecutor {
 		 * Drives '/rr leave'.  Allows player to leave tower and religion
 		 */
 		else if(cmd.getName().equalsIgnoreCase("rr") && arg[0].equals("leave") && arg.length == 1){
-			if(!Religion.info.hasPlayer((Player)sender)){
+			if(!BeyondInfo.hasPlayer((Player)sender)){
 				sender.sendMessage("YOU ARENT PART OF A RELIGION YOU CUNT");
 				return true;
 			}
-			Religion.info.removePlayer((Player)sender);
+			BeyondInfo.removePlayer((Player)sender);
 			sender.sendMessage("You have left your tower and religion");
 			return true;
 		}
@@ -86,15 +85,15 @@ public class CommandRunner implements CommandExecutor {
 		 * Drives '/rr list'.  Lists to player all religions that exist
 		 */
 		else if(cmd.getName().equalsIgnoreCase("rr") && arg[0].equals("list") && arg.length == 1){
-			sender.sendMessage("The religions that exist are: "+Religion.info.getReligions().toString());
+			sender.sendMessage("The religions that exist are: "+BeyondInfo.getReligions().toString());
 			return true;
 		}
 		
 		/**
 		 * Drives '/rr <religion> list' Lists all towers associated with param religion
 		 */
-		else if(cmd.getName().equalsIgnoreCase("rr") && Religion.info.getReligions().contains(arg[0]) && arg[1].equals("list") && arg.length == 2){
-			sender.sendMessage("The towers that exist are: "+Religion.info.getTowers(arg[0]));
+		else if(cmd.getName().equalsIgnoreCase("rr") && BeyondInfo.getReligions().contains(arg[0]) && arg[1].equals("list") && arg.length == 2){
+			sender.sendMessage("The towers that exist are: "+BeyondInfo.getTowers(arg[0]));
 			return true;
 		}
 		
@@ -103,10 +102,10 @@ public class CommandRunner implements CommandExecutor {
 		 * Drives '/rr details <towername>'  Gets general details of a tower.
 		 */
 		else if(cmd.getName().equalsIgnoreCase("rr") && arg[0].equals("details") && arg.length == 2){
-			if(Religion.info.hasTower(arg[1])){
+			if(BeyondInfo.hasTower(arg[1])){
 				sender.sendMessage("General Info");
 			}else{
-				sender.sendMessage("This is not a valid tower. The towers that exist are: "+Religion.info.getTowers(arg[0]));
+				sender.sendMessage("This is not a valid tower. The towers that exist are: "+BeyondInfo.getTowers(arg[0]));
 			}
 			return true;
 		}
@@ -116,7 +115,7 @@ public class CommandRunner implements CommandExecutor {
 		 *Drives '/rr details' Gets in-depth details about a player specific religion
 		 */
 		else if(cmd.getName().equalsIgnoreCase("rr") && arg[0].equals("details") && arg.length == 1){
-			if(Religion.info.hasPlayer((Player)sender)){
+			if(BeyondInfo.hasPlayer((Player)sender)){
 				sender.sendMessage("In-depth Info");
 			}else{
 				sender.sendMessage("This command is invalid because you are not a member of a tower");
@@ -129,7 +128,7 @@ public class CommandRunner implements CommandExecutor {
 		 * Drives '/rr here'.  Outputs the territory you are standing in.
 		 */
 		else if(cmd.getName().equalsIgnoreCase("rr") && arg[0].equals("here") && arg.length == 1){
-			sender.sendMessage("You are in the territory of "+Religion.info.getTower(((Player)sender).getLocation()));
+			sender.sendMessage("You are in the territory of "+BeyondInfo.getTower(((Player)sender).getLocation()));
 			return true;
 		}
 		
@@ -138,11 +137,11 @@ public class CommandRunner implements CommandExecutor {
 		 * Drives '/rr who'. Lists all members of players religion who are online in white, and all members associated with players tower who are online in red 
 		 */
 		else if(cmd.getName().equalsIgnoreCase("rr") && arg[0].equals("who") && arg.length == 1){
-			if(Religion.info.getReligion((Player)sender) == null) ((Player)sender).sendMessage("You are not a member of a Tower and are thus unable to use this command");
+			if(BeyondInfo.getReligion((Player)sender) == null) ((Player)sender).sendMessage("You are not a member of a Tower and are thus unable to use this command");
 			String message = "";
 			List<Player> onlineplayers = Arrays.asList(plugin.getServer().getOnlinePlayers());
-			List<Player> towerplayers = Religion.info.getTowerPlayers(Religion.info.getTowerName((Player)sender));
-			List<Player> religionplayers = Religion.info.getReligionPlayers(Religion.info.getReligion((Player)sender));
+			List<Player> towerplayers = BeyondInfo.getTowerPlayers(BeyondInfo.getTowerName((Player)sender));
+			List<Player> religionplayers = BeyondInfo.getReligionPlayers(BeyondInfo.getReligion((Player)sender));
 			for(int i=0;i<religionplayers.size();i++){
 				if(towerplayers.contains(religionplayers.get(i)) && onlineplayers.contains(religionplayers.get(i))) message = message.concat(ChatColor.RED.toString()+religionplayers.get(i).getName());
 				else if(onlineplayers.contains(religionplayers.get(i))) message = message.concat(ChatColor.WHITE.toString()+religionplayers.get(i).getName());
