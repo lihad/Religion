5fea6165318d0bc3f12e1502f697ece65a1fcb98
Joren <GitHub.Joren@Joren.me>
2012-02-09 11:51:05 +0900

    Try to make it so that tower names contained in the list of religions get rejected.  How about just booting the player from their previous tower if they make a new one?

--- src/Lihad/Religion/Listeners/BeyondBlockListener.java
+++ src/Lihad/Religion/Listeners/BeyondBlockListener.java
@@ -16,13 +16,15 @@ public class BeyondBlockListener extends BlockListener {
 	/**
 	 * TODO: Make it so a tower can not be named after a religion.
 	 * TODO: Make it so only those without a religion/tower can start a new tower
+	 * 
+	 * How about just make it so that if you're in a tower, you get booted when you make a new one?
 	 */
 	public void onSignChange(SignChangeEvent event){
 		if(event.getBlock().getLocation().getBlockY() > 119){
 			if(event.getLine(0).equals("[Religion]") && !(event.getLine(1).equals(null))&& !(event.getLine(2).equals(null))){
 				if(BeyondInfo.getReligions().contains(event.getLine(1))){
-					if(BeyondInfo.hasTower(event.getLine(2))){
-						event.getPlayer().sendMessage("This tower already exist. Try a different name.");
+					if(BeyondInfo.hasTower(event.getLine(2))||BeyondInfo.getReligions().contains(event.getLine(2))){
+						event.getPlayer().sendMessage("This name is already being used - try a different one.");
 						return;
 					}
 					BeyondInfo.addTower(event.getPlayer(), event.getLine(1), event.getLine(2), event.getBlock().getLocation());
